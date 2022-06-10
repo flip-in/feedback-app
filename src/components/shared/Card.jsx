d98f4277a1e37e51e032f8f519953987facb6e1d
William Price <pricew86@gmail.com>
2022-06-10 12:10:23 +0800

    added delete feedback

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,8 +1,28 @@
-function Card({children}) {
+import PropTypes from 'prop-types'
+
+function Card({ children, reverse }) {
+  /* example of conditional class assignment */
+  // return (<div className={`card ${reverse && 'reverse'}`}>{children}</div>)
+
+/* example of conditional styling*/
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff', color: reverse ? '#fff' : '#000'
+      }}
+    >
       {children}
     </div>
   )
+}
+
+Card.defaultProps = {
+  reverse: false,
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
 }
 export default Card
