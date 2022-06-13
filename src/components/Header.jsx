f39de1fbb24ad8dd387f267974dd8db28cb1f04c
William Price <pricew86@gmail.com>
2022-06-13 15:37:45 +0800

    added links and routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,38 @@
-import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function Header({text, bgColor, textColor}) {
-
+function Header({ text, bgColor, textColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
-  }
+  };
+
+  const linkStyle = {
+    textDecoration: "none",
+    color: "inherit",
+  };
 
   return (
     <header style={headerStyles}>
       <div className="container">
-        <h2>{text}</h2>
+        <Link to="/" style={linkStyle}>
+          <h2>{text}</h2>
+        </Link>
       </div>
     </header>
-  )
+  );
 }
 
 Header.defaultProps = {
-  text: 'FeedbackUI',
-  bgColor: 'rgba(0,0,0,0.4)',
-  textColor: '#ff6a95' 
-}
+  text: "FeedbackUI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
 
 Header.propTypes = {
   text: PropTypes.string,
   bgColor: PropTypes.string,
   textColor: PropTypes.string,
-}
+};
 
-export default Header
+export default Header;
